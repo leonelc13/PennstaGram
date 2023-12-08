1734f940a571ae88b373d6a2a6f3662cbd01dd00
leonelc13 <112787135+leonelc13@users.noreply.github.com>
2023-12-07 20:02:13 -0500

    update

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,4 +1,4 @@
-export const serverPort = process.env.PORT;
+export const serverPort = process.env.PORT || 3000;
 export const socketPort = 8000;
 export const serverURL = 'http://localhost';
 export const rootURL = 'http://localhost';
